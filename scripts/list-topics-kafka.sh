95981afac18c3857c18c4fafda1f47df30dda046
Sam Zagrobelny <strangesast@gmail.com>
2020-07-10 20:37:21 +0000

    fixed kafka listener configuration

--- scripts/list-topics-kafka.sh
+++ scripts/list-topics-kafka.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --rm -it --network=core_default wurstmeister/kafka /opt/kafka/bin/kafka-topics.sh \
-  --bootstrap-server kafka:9092 \
+docker run --rm -it --network=host wurstmeister/kafka /opt/kafka/bin/kafka-topics.sh \
+  --bootstrap-server localhost:9092 \
   --list
